edea8496aa7a751a7cd34e614d82687dfe9c4c67
Sergiu Ghiran <sergiughiran@Sergius-MacBook-Pro.local>
2023-10-27 13:16:55 +0300

    • Created and signed project

---
+++ TikTok Reporter/TikTok Reporter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TikTok Reporter
+//
+//  Created by Sergiu Ghiran on 27.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TikTok Reporter/TikTok Reporter/TikTok_ReporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTok_ReporterApp.swift
+//  TikTok Reporter
+//
+//  Created by Sergiu Ghiran on 27.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TikTok_ReporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
