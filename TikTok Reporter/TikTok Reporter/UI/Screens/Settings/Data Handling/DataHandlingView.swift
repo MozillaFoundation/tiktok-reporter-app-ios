7a877928c88dcd5adc6cfd5e774cff6afedc01bd
Emrah Korkmaz <emrahkrkmz1@gmail.com>
2024-01-23 15:38:50 +0200

    [bugfix/markdown-and-text-changes] add custom alert for data handling screen ui

--- TikTok Reporter/TikTok Reporter/UI/Screens/Settings/Data Handling/DataHandlingView.swift
+++ TikTok Reporter/TikTok Reporter/UI/Screens/Settings/Data Handling/DataHandlingView.swift
@@ -40,17 +40,59 @@ struct DataHandlingView: View {
             VStack(spacing: .m) {
 
                 MainButton(text: Strings.downloadTitle, type: .secondary) {
+                    
                     viewModel.requestDataDownload()
                 }
 
-                MainButton(text: Strings.deleteTitle, type: .secondary) {
-                    viewModel.requestDataDelete()
+                if !viewModel.isUserDataDeleted {
+                    MainButton(text: Strings.deleteTitle, type: .secondary) {
+                        viewModel.requestDataDelete()
+                    }
                 }
+                
+                if viewModel.isUserDataDeleted {
+                    VStack {
+                        HStack {
+                            Spacer()
+                            
+                            Image(systemName: "checkmark")
+                                .renderingMode(.template)
+                                .foregroundColor(.success)
+                            
+                            Text(Strings.dataDeletionTitle)
+                                .foregroundStyle(Color.success)
+                                .font(.body3)
+                            
+                            Spacer()
+                        }
+                    }.padding(.top, 5)
+                }
+
             }
 
             Spacer()
         }
         .padding(.l)
+        .customAlert(title: Strings.noEmailAlertTitle,
+                     description: Strings.noEmailAlertDescription,
+                     isPresented: $viewModel.routing.noEmailAlert) {
+            MainButton(text: Strings.noEmailAlertActionTitle, type: .secondary) {
+                viewModel.routing.noEmailAlert = false
+            }
+        }
+        .customAlert(title: Strings.deleteDataAlertTitle,
+                     description: Strings.deleteDataAlertDescription,
+                     isPresented: $viewModel.routing.deleteDataAlert,
+                     secondaryButton: {
+            MainButton(text: Strings.deleteDataAlertSecondaryActionTitle, type: .secondary) {
+                viewModel.routing.deleteDataAlert = false
+            }
+        }, primaryButton: {
+            MainButton(text: Strings.deleteDataAlertPrimaryActionTitle, type: .primary) {
+                viewModel.routing.deleteDataAlert = false
+                viewModel.deleteUserData()
+            }
+        })
     }
 }
 
@@ -66,4 +108,12 @@ private enum Strings {
     static let title = "Data Handling"
     static let downloadTitle = "Download My Data"
     static let deleteTitle = "Delete My Data"
+    static let noEmailAlertTitle = "No email provided"
+    static let noEmailAlertDescription = "Please provide an email in order to get a copy of your data."
+    static let noEmailAlertActionTitle = "Got it"
+    static let deleteDataAlertTitle = "Delete Data?"
+    static let deleteDataAlertDescription = "Are you sure you want to delete all your data from the system?"
+    static let deleteDataAlertPrimaryActionTitle = "Delete"
+    static let deleteDataAlertSecondaryActionTitle = "No"
+    static let dataDeletionTitle = "Data Succesfully Deleted"
 }
