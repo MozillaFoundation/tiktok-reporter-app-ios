ce5866f671d41d2f06f3268ddf49608c915d5800
Paul Craciunoiu <paul@uplift.ltd>
2024-04-01 18:13:07 -0600

    Remove option to request a copy of report.

--- TikTok Reporter/TikTok Reporter/UI/Views/SubmissionSuccessView/SubmissionSuccessView.swift
+++ TikTok Reporter/TikTok Reporter/UI/Views/SubmissionSuccessView/SubmissionSuccessView.swift
@@ -53,36 +53,6 @@ struct SubmissionSuccessView: View {
                     postCloseNotificationForShareExt()
                 }
             }
-
-            
-            if !isPresentedFromShareExtension {
-                
-                VStack(alignment: .leading) {
-                    Text(Strings.settingDescription)
-                        .fixedSize(horizontal: false, vertical: true)
-                        .font(.body2)
-                        .foregroundStyle(.disabled)
-
-                    NavigationLink {
-                        EmptyView()
-                    } label: {
-                        HStack {
-                            Button(action: {
-                                NotificationCenter.default.post(name: NSNotification.Name(Strings.settingActionNotificationName), object: nil)
-                            }) {
-                                Text(Strings.settingButtonTitle)
-                                    .foregroundColor(.basicRed)
-                                    .font(.body2)
-                                    .underline()
-                            }
-                        }
-                    }
-                    
-                    
-                }
-                
-            }
-                        
         }
         .padding(.horizontal, .xl)
     }
@@ -111,7 +81,6 @@ private extension SubmissionSuccessView {
 
 private enum Strings {
     static let title = "Thank you!\nReport submitted."
-    static let description = "To receive a copy of your report on your email and to get follow up information about our study, go to Settings"
     static let buttonTitle = "I'm done"
     static let settingDescription = "To receive a copy of your report on your email and to get follow up information about our study, go to "
     static let settingButtonTitle = "Settings"
